3bf6f79a61468280d99bfcfff03ed2fd53c8e043
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-10-13 13:00:38 -0400

    Add EF Core DbContext and configure database connection
    Updated `InsuranceManager.csproj` with a non-visible character, likely due to encoding. Added `ConnectionStrings` to `appsettings.json` for a local SQL Server database (`InsuranceManagerDB`) and reformatted `Logging` and `AllowedHosts`. Introduced `ApplicationDbContext` in `ApplicationDbContext.cs` to manage `Customer`, `Policy`, `CustomerPolicy`, and `Claim` entities using Entity Framework Core.

---
+++ InsuranceManager/InsuranceManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using InsuranceManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InsuranceManager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Policy> Policies {get; set;}
+        public DbSet<CustomerPolicy> CustomerPolicies { get; set;}
+        public DbSet<Claim> Claims { get; set; }
+    }
+}
