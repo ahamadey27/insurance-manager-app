5b53f56a5d1b30bceb656ea9bf093fcce45ff782
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-10-20 11:17:54 -0400

    Create SeedData.cs

---
+++ InsuranceManager/InsuranceManager/Data/SeedData.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace InsuranceManager.Data
+{
+    public static class SeedData
+    {
+        public static async Task Initialize(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManger = serviceProvider.GetRequiredService<RoleManager<IdentityUser>>();
+
+            //Define Roles
+            List<string> roleNames = new List<string> {"Admin", "User"};
+            IdentityResult roleResult;
+
+        }
+    }
+}
